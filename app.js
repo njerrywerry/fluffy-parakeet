ad1df294115952455312b9444644a35cd36101f8
Sylvia <sylviakieha@gmail.com>
2016-02-16 22:57:56 +0300

    angularjs services/factories

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 angular.module('myApp', ['ngRoute'])
-.directive('hello', [function() {
-  return {
-    restrict: 'CEMA', // C: class, E: element, M: comments, A: attributes
-    replace: true, // replaces original content with template
-    template: '<span><br>Hello</span'
-  };
-}])
-.controller('TodoController', ['$scope', function ($scope) {
-  $scope.todos = [
-    { name: 'Do Dishes', completed: false },
-    { name: 'Do Laundry', completed: false },
-    { name: 'Learn Angular', completed: true },
-    { name: 'Write Notes', completed: false },
-    { name: 'Do Assignments', completed: false },
-    { name: 'Be awesome!', completed: false },
+
+.factory('Todos', function() {
+  return [
+    { name: 'Do Dishes', completed: false, note: 'add notes...' },
+    { name: 'Do Laundry', completed: false, note: 'add notes...' },
+    { name: 'Learn Angular', completed: true, note: 'add notes...' },
+    { name: 'Write Notes', completed: false, note: 'add notes...' },
+    { name: 'Do Assignments', completed: false, note: 'add notes...' },
+    { name: 'Be awesome!', completed: true, note: 'add notes...' },
   ];
+})
+
+.controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
+  $scope.todos = Todos;
 }])
+
+.controller('TodoDetailCtrl', ['$scope', '$routeParams', 'Todos', function ($scope, $routeParams, Todos) {
+  $scope.todo = Todos[$routeParams.id];
+}])
+
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
   .when('/', {
     templateUrl: '/todos.html',
     controller: 'TodoController'
+  })
+
+  .when('/:id', {
+    templateUrl: '/todoDetails.html',
+    controller: 'TodoDetailCtrl'
   });
 }]);
